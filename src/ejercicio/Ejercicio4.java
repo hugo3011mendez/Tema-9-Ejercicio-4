edfe907b8c3b38ae97a9b09b304fdaa75363e4e4
Hugo Mendez <hugo3011mendez@gmail.com>
2020-05-28 10:20:08 +0200

    Estructura creada

---
+++ src/ejercicio/Ejercicio4.java
@@ -0,0 +1,14 @@
+package ejercicio;
+
+import javax.swing.*;
+
+public class Ejercicio4{
+    public static void main(String[] args) {
+        FrameEjercicio4 frame = new FrameEjercicio4();
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 350);
+        frame.setVisible(true);
+    }
+}
---
+++ src/ejercicio/FrameEjercicio4.java
@@ -0,0 +1,15 @@
+package ejercicio;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.File;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import javax.swing.filechooser.*;
+
+public class FrameEjercicio4 extends JFrame implements ActionListener{
+    
+}
